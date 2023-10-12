cdcd1d3c69a5f557a689860c83d6ca4d74b3694b
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-10-12 14:51:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+
+  class Dinner {
+    #dessert;
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      // Initialize dessert as a private property
+      this.#dessert = dessert;
+    }
+  
+    // Getter method to access the private dessert property
+    getDessert() {
+      return this.#dessert;
+    }
+  }
+  
+  
